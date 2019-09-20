3acd1545d183fa27c4062a33c5b9b9ff496c8a86
kotasuzuki <kota.suzuki@ucl.meijo-u.ac.jp>
2019-09-21 00:44:36 +0900

    html/temp

--- studyGolang/main.go
+++ studyGolang/main.go
@@ -6,16 +6,13 @@ import (
 	"net/http"
 )
 
-
 func htmlHandler0(w http.ResponseWriter, r *http.Request) {
-	
-type Information struct {
-	Index string
-	En1   string
-	En2   string
-}
-
-var tunnels = make(map[[16]byte]Information) //マップ型変数
+	type Information struct {
+		Index string
+		En1   string
+		En2   string
+	}
+	var tunnels = make(map[[16]byte]Information) //マップ型変数
 	tunnels[[16]byte{0}] = Information{
 		Index: "1",
 		En1:   "192.168.100.1",
@@ -27,16 +24,16 @@ var tunnels = make(map[[16]byte]Information) //マップ型変数
 		En2:   "192.168.200.4",
 	}
 	// テンプレートをパース
-	t := template.Must(template.ParseFiles("temp.html"))
+	t := template.Must(template.ParseFiles("temp.hpl"))
 
-	if err := t.ExecuteTemplate(w, "temp.html", tunnels); err != nil {
+	/*HTMLファイルにレンダリング*/
+	if err := t.ExecuteTemplate(w, "temp.hpl", tunnels); err != nil {
 		log.Fatal(err)
 	}
-
 }
 
 func main() {
 	http.HandleFunc("/page0", htmlHandler0)
 	// サーバーを起動
-	http.ListenAndServe(":8989", nil)
+	http.ListenAndServe(":8080", nil)
 }
